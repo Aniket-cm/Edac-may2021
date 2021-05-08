185d390bc842d0f66e01d7249079876dc302a75f
Aniket-cm <83497533+Aniket-cm@users.noreply.github.com>
2021-05-08 16:42:54 +0530

    Add files via upload

---
+++ Assignment1/Program_1.java
@@ -0,0 +1,9 @@
+public class Program_1 {
+    public static void main(String[] args)
+    {
+
+        System.out.println("Hello");
+
+        System.out.println("Aniket Darunde");
+    }
+}
---
+++ Assignment1/Program_7.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Program_7 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a First Number :");
+
+        int num1 = scan.nextInt();
+
+        for(int i=1;i<=3;i++) {
+            System.out.println("multiplicarion :"+(i*num1));
+        }
+
+
+    }
+}
